0ac6815d824c0e98a9ec71d61ea41b0ebe5b58bf
Søren Berg Glasius <soeren@glasius.dk>
2025-08-15 12:51:20 +0200

    Initial commit.

---
+++ grails-app/init/grails/spring/security/oauth/oidc/BootStrap.groovy
@@ -0,0 +1,15 @@
+package grails.spring.security.oauth.oidc
+
+import jakarta.servlet.ServletContext
+
+class BootStrap {
+
+    ServletContext servletContext
+
+    def init = {
+    }
+
+    def destroy = {
+    }
+
+}
